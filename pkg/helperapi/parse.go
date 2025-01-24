c0410eeb2ce45894bc8b574343230293e1186262
Ben Fiola <benfiola@gmail.com>
2025-01-23 22:51:26 -0800

    feat: add parsing utilities

---
+++ pkg/helperapi/parse.go
@@ -0,0 +1,103 @@
+package helperapi
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Unmarshals a file into the provided struct pointer.
+// Returns an error if unmarshalling fails.
+// Returns an error if the file type is not recognized.
+func (api *Api) UnmarshalFile(file string, data any) error {
+	if strings.HasSuffix(file, ".json") {
+		return api.UnmarshalJsonFile(file, data)
+	} else if strings.HasSuffix(file, ".xml") {
+		return api.UnmarshalXmlFile(file, data)
+	} else {
+		return fmt.Errorf("unrecognized file type %s", file)
+	}
+}
+
+// Marshals data into the given file
+// Returns an error if marshalling fails.
+// Returns an error if the file type is not recognized.
+func (api *Api) MarshalFile(data any, file string) error {
+	if strings.HasSuffix(file, ".json") {
+		return api.MarshalJsonFile(data, file)
+	} else if strings.HasSuffix(file, ".xml") {
+		return api.MarshalXmlFile(data, file)
+	} else {
+		return fmt.Errorf("unrecognized file type %s", file)
+	}
+}
+
+// Unmarshals a JSON file into the provided struct pointer.
+// Returns an error if the file is unreadable.
+// Returns an error if the file's contents is not JSON encoded.
+func (api *Api) UnmarshalJsonFile(file string, data any) error {
+	fileBytes, err := os.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(fileBytes, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Marshals data into JSON and writes it to the given file.
+// Returns an error if the data could not be JSON encoded.
+// Returns an error if the file is not writeable
+func (api *Api) MarshalJsonFile(data any, file string) error {
+	dataBytes, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(file, dataBytes, 0755)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Unmarshals an XML file into the provided struct pointer.
+// Returns an error if the file is unreadable.
+// Returns an error if the file's contents is not XML encoded.
+func (api *Api) UnmarshalXmlFile(file string, data any) error {
+	fileBytes, err := os.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	err = xml.Unmarshal(fileBytes, data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Marshals data into XML and writes it to the given file.
+// Returns an error if the data could not be XML encoded.
+// Returns an error if the file is not writeable
+func (api *Api) MarshalXmlFile(data any, file string) error {
+	dataBytes, err := xml.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(file, dataBytes, 0755)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}

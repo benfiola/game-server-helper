2f9c286f8dbde3954084470592d919161b7c1ffc
Ben Fiola <benfiola@gmail.com>
2025-01-30 21:52:45 -0800

    fix: modify download to download to path rather than callback with temp dir

--- pkg/download.go
+++ pkg/download.go
@@ -6,41 +6,28 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"path/filepath"
 )
 
-// downloadCb is a callback invoked with a temporary path pointing to a downloaded file
-type downloadCb func(path string) error
-
-// Downloads a url to a temporary file and invokes a callback with the path to the temporary file.
+// Downloads a url to the target path
 // Returns an error if the download fails.
-// Returns an error if the callback returns an error.
-func Download(ctx context.Context, url string, cb downloadCb) error {
-	return CreateTempDir(ctx, func(tempDir string) error {
-		baseName := filepath.Base(url)
-		tempFile := filepath.Join(tempDir, baseName)
-		handle, err := os.Create(tempFile)
-		if err != nil {
-			return err
-		}
-		defer handle.Close()
+func Download(ctx context.Context, url string, dest string) error {
+	handle, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer handle.Close()
 
-		Logger(ctx).Info("download", "url", url, "file", tempFile)
-		response, err := http.Get(url)
-		if err != nil {
-			return err
-		}
-		defer response.Body.Close()
-		if response.StatusCode != http.StatusOK {
-			return fmt.Errorf("GET %s sent non-200 status code: %d", url, response.StatusCode)
-		}
+	Logger(ctx).Info("download", "url", url, "file", dest)
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("GET %s sent non-200 status code: %d", url, response.StatusCode)
+	}
 
-		chunkSize := 1024 * 1024
-		_, err = io.CopyBuffer(handle, response.Body, make([]byte, chunkSize))
-		if err != nil {
-			return err
-		}
-
-		return cb(tempFile)
-	})
+	chunkSize := 1024 * 1024
+	_, err = io.CopyBuffer(handle, response.Body, make([]byte, chunkSize))
+	return err
 }
